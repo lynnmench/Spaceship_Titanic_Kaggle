f5af74ab924acb1d2edf7c013bb5b0115b548874
lynnpowell <powell.lynnm@gmail.com>
2022-04-15 07:04:13 -0500

    initial commit

---
+++ DataClean_SpaceshipTitanic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created: 15Apr2022
+Author: Lynn Menchaca
+
+Resources:
+Kaggle Competition-> Spaceship Titanic
+
+Purpose: Cleaning Data Files
+"""
+
