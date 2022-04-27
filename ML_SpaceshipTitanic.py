969a6e762a28f1bebeea4f58cdb246273345dbf6
lynnpowell <powell.lynnm@gmail.com>
2022-04-27 15:50:05 -0500

    Continued with initial analysis. Started feature selection for ML

--- ML_SpaceshipTitanic.py
+++ ML_SpaceshipTitanic.py
@@ -9,6 +9,13 @@ Author: Abhini Shetye
 Title: Feature Selection with sklearn and Pandas
 https://towardsdatascience.com/feature-selection-with-pandas-e3690ad8504b
 """
+"""
+From the data cleaning and initial analysis file the features that look
+like they would play an impact:
+
+Age_Infant, HomePlanet_Earth, HomePlanet_Europa, CryoSleep,
+Cabin_Deck_B,C,E,F, Cabin_Side_S
+"""
 
 import pandas as pd
 import numpy as np
@@ -20,19 +27,37 @@ df_train = pd.read_csv(data_file_path+'Analysis_Train_SpaceTitanic.csv')
 df_test = pd.read_csv(data_file_path+'Analysis_Train_SpaceTitanic.csv')
 
 #Using Pearson Correlation to see feature relations to Transported
-#plt.figure(figsize=(12,10))
+#Also refered to as the Filter Method in Feature Selection with sklearn and Pandas
+#Comparing correlation feature values to target column
 df_cor = df_train.corr()
-#sns.heatmap(df_cor, annot=True, cmap=plt.cm.Reds)
-#plt.show()
-#To many columns to acuratly see results
-
-#Comparing features to target column
 cor_target = abs(df_cor['Transported'])
 
 #Selecting highly correlated features
-pearson_relevant_features = cor_target[cor_target>0.2]
-df_pearson_relevant = df_train[pearson_relevant_features.index.values.tolist()]
+relevant_features = cor_target[cor_target>0.2]
+relevant_features_list = relevant_features.index.values.tolist()
+df_relevant = df_train[relevant_features_list]
 #Compare features to each other to avoid overfitting model
 #This time dropping the features with high correlation values
-pearson_cor = df_pearson_relevant[pearson_relevant_features.index.values.tolist()].corr()
+relevant_cor = df_relevant[relevant_features_list].corr()
+cor_relevant_high = []
+
+for feat_cor in relevant_features_list:
+    print(feat_cor)
+    cor_compare = abs(relevant_cor[feat_cor])
+    relev_high = cor_compare[cor_compare>0.3]
+    relev_high_list = relev_high.index.values.tolist()
+    cor_relevant_high.append(relev_high_list)
+    print('Features with high correlation to {}: '.format(feat_cor))
+    print(relev_high_list)
+
+#Looking at the relevant_cor data
+#Cryosleep has high correlation with locations money is spent.
+#This makes sense becuase if the passenger choose to be put in 
+#suspended animation they can't spend money.
+#However to consider the people awake I will keep CryoSleep feature
+#Final list of pearson features with low correlation to the other features
+pearson_features = ['CryoSleep', 'RoomService_0', 'FoodCourt_0',
+                    'ShoppingMall_0', 'Spa_0', 'VRDeck_0']
+
+
 
