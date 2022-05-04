6e5e7713bea90121e1b87e991238e783285a1abd
lynnpowell <powell.lynnm@gmail.com>
2022-05-03 20:15:15 -0500

    reducing columns and rows

--- ML_SpaceshipTitanic.py
+++ ML_SpaceshipTitanic.py
@@ -20,6 +20,9 @@ Cabin_Deck_B,C,E,F, Cabin_Side_S
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.datasets import load_breast_cancer
+from sklearn.feature_selection import RFECV
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 
 data_file_path = '/Users/lynnpowell/Documents/DS_Projects/Spaceship_Titanic/'
@@ -31,9 +34,6 @@ df_test = pd.read_csv(data_file_path+'Analysis_Train_SpaceTitanic.csv')
 #Comparing correlation feature values to target column
 
 df_copy = df_train.copy()
-
-#clean data frame missing collumns
-miss_col = ['']
 
 
 df_cor = df_train.corr()
@@ -70,14 +70,49 @@ pearson_features = ['CryoSleep', 'Spend_Sum_$0']
 #with spending and cryo sleep.
 #This method would work better if the features were more independent of each other
 
-#Dropping features to avail
-#Drop features with high correlation to each other and not needed features
-high_cor_feat = ['']
-df_train = df_train.drop(high_cor_feat,axis=1)
-df_test = df_test.drop('Name',axis=1)
+#Removing missing rows from: 
+#Age, RoomService, Spa, VRDeck, Cabin, Homeplanet, CryoSleep
+#Continue to consider some of their features
+drop_miss_cat = ['HomePlanet', 'Cabin']
+drop_miss_num = ['CryoSleep', 'Age','RoomService', 'Spa', 'VRDeck']
+
+print(df_copy.shape)
+#rows=8693, col=50
+#df_copy['HomePlanet'].value_counts().Missing
+#HomePlanet = 201 Missing
+#df_copy['Cabin'].value_counts().Missing
+#Cabin = 199 Missing
+df_low = df_copy[(df_copy.HomePlanet != 'Missing') & (df_copy.Cabin != 'Missing')]
+print(df_low.shape)
+#rows=8299, col=50, deleted rows = 394
+
+df_low = df_low[df_low['CryoSleep'] != -1]
+for feat in drop_miss_num:
+    df_low = df_low[df_low[feat] != -0.5]
+print(df_low.shape)
+#rows7594, col=50, deleted rows = 705
+
+df_low['RoomService'].min()
+
+##Dropping features low correlation to transported features
+#Dropping features with high correlation to other features
+    #To avoid overfitting
+drop_cor_feat = ['PassengerId', 'HomePlanet', 'Cabin', 'Destination', 'Age',
+       'VIP', 'FoodCourt', 'ShoppingMall','Age_Categories', 'Spend_Sum',
+       'RoomService_0', 'FoodCourt_0', 'ShoppingMall_0', 'Spa_0', 'VRDeck_0',
+       'Cabin_Deck', 'Cabin_Side', 'HomePlanet_Missing', 'Destination_55 Cancri e',
+       'Destination_Missing', 'Destination_PSO J318.5-22',
+       'Destination_TRAPPIST-1e', 'Age_Missing', 'Age_Child',
+       'Age_Teenager', 'Age_Young Adult', 'Age_Adult', 'Age_Senior',
+       'Cabin_Deck_A', 'Cabin_Deck_D','Cabin_Deck_G', 'Cabin_Deck_Missing',
+       'Cabin_Deck_T', 'Cabin_Side_Missing', 'Cabin_Side_S']
+df_low = df_low.drop(drop_cor_feat,axis=1)
+print(df_low.shape)
+#rows=7594, col=15, deleted cols = 35
+
 
 #Wrapper Method
-
+df_low.columns
 
 
 
