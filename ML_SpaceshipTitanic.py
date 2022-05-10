c0349ce2fd821a0d586f64132c8bf290bd184c83
lynnpowell <powell.lynnm@gmail.com>
2022-05-10 06:19:06 -0500

    Cleaned spending bins,updated feature selection, evaluated df with 4 ML models

--- ML_SpaceshipTitanic.py
+++ ML_SpaceshipTitanic.py
@@ -20,13 +20,20 @@ Cabin_Deck_B,C,E,F, Cabin_Side_S
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import minmax_scale
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import load_breast_cancer
 from sklearn.feature_selection import RFECV
 from sklearn.ensemble import RandomForestClassifier
-#import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
+import statsmodels.api as sm
+
 
 #Reading in data files created from Data_Clean_Analyze_SpaceshipTitanic
 data_file_path = '/Users/lynnpowell/Documents/DS_Projects/Spaceship_Titanic/'
@@ -35,24 +42,112 @@ df_test = pd.read_csv(data_file_path+'Analysis_Train_SpaceTitanic.csv')
 
 #df_copy = df_train.copy()
 
-#Functions Logistic Regression accuracy with cross validation
+#Function: Logistic Regression accuracy with cross validation
 def model_accuracy(model, df_train, target):
     train_X = df_train
     target_y = target
     #Using Logist Regression to evaluate by feature selections
-    scores = cross_val_score(model, train_X, target_y, cv=10)
-    accuracy = np.median(scores)
+    scores = cross_val_score(model, train_X, target_y,
+                             scoring = 'neg_mean_absolute_error',cv=10)
+    accuracy = np.mean(scores)
     return accuracy
 
+
+#Function: Test data set on multiple models
+#Linear Regression, Lasso Regression (Alpha Search),
+#Random Forest Classifier (Grid Search), K-Nearest Neighbor (Grid Search)
+def ML_Model_FullDF(train, target, set_name):
+    accuracy_list = []
+    accuracy_index = []
+    i=0
+    model_list = []
+    model_name = []
+    
+    #Linear Regression
+    linr = LinearRegression()
+    model_list.append(linr)
+    model_name.append(set_name+' Linear Regression')
+    
+    #Random Forest Classifier (grid search)
+    rfc_hyperpar = {
+        'criterion' : ['entropy', 'gini'],
+        'max_depth' : [5, 10],
+        'max_features' : ['log2', 'sqrt'],
+        'min_samples_leaf' : [1,5],
+        'min_samples_split' : [3,5],
+        'n_estimators' : [6,9]
+    }
+    rfc = RandomForestClassifier(random_state=7)
+    grid = GridSearchCV(rfc, param_grid=rfc_hyperpar, cv=10)
+    grid.fit(train, target)
+    #rfc_best_params = grid.best_params_
+    #rfc_best_score = grid.best_score_
+    best_rfc = grid.best_estimator_
+    model_list.append(best_rfc)
+    model_name.append(set_name+' Random Forest Classifier')
+    
+    #K-Nearest Neighbors
+    knn_hyperpar = {
+        "n_neighbors": range(1,20,2),
+        "weights": ["distance", "uniform"],
+        "algorithm": ['brute'],
+        "p": [1,2]
+    }
+    knn = KNeighborsClassifier()
+    grid = GridSearchCV(knn, param_grid=knn_hyperpar, cv=10)
+    grid.fit(x_train, target)
+    #knn_best_params = grid.best_params_
+    #knn_best_score = grid.best_score_
+    best_knn = grid.best_estimator_
+    model_list.append(best_knn)
+    model_name.append(set_name+' K Nearest Neighbors')
+    
+    for ml_model in model_list:
+        accuracy_list.append(model_accuracy(ml_model,train, target))
+        accuracy_index.append(model_name[i])
+        i+=1
+    
+    return accuracy_list, accuracy_index
+'''
+    #Lasso Regression with alpha search
+    #default alpha=1, best alpha=
+    lml = Lasso(alpha=0.1)
+    print(model_accuracy(lml,x_train, target))
+
+    alpha = []
+    error = []
+
+    for i in range(1,100):
+        alpha.append(i/1000)
+        lml = Lasso(alpha=(i/1000))
+        error.append(model_accuracy(lml,x_train, target))
+
+    plt.plot(alpha, error)
+    err = tuple(zip(alpha, error))
+    df_err = pd.DataFrame(err, columns = ['alpha', 'error'])
+    print(df_err[df_err.error == max(df_err.error)])
+    model_acc.append(model_accuracy(lml,x_train, target))
+    model_index.append('Init Lasso Regression')
+
+    '''
+    
+
+
 #To keep track of Logistic Regression with each feature
-model_accuracy = []
+model_acc = []
 model_index = []
 
 
 #Looking just at the spending features
 spending_list = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck',
-                 'Spend_Sum', 'Spend_Sum_0', 'RoomService_0','FoodCourt_0',
-                 'ShoppingMall_0', 'Spa_0', 'VRDeck_0']
+                 'Spend_Sum', 'Spend_Sum_0', 'RoomService_$0.1_2000', 
+                 'RoomService_$2001_30000', 'FoodCourt_Missing',
+       'FoodCourt_$0', 'FoodCourt_$0.1_2000', 'FoodCourt_$2001_30000',
+       'ShoppingMall_Missing', 'ShoppingMall_$0', 'ShoppingMall_$0.1_2000',
+       'ShoppingMall_$2001_30000', 'Spa_Missing', 'Spa_$0', 'Spa_$0.1_2000',
+       'Spa_$2001_30000', 'Spend_Sum_Missing',
+       'Spend_Sum_$0.1_1000', 'Spend_Sum_1001_3000', 'Spend_Sum_$3001_30000',
+       'VRDeck_Missing', 'VRDeck_$0', 'VRDeck_$0.1_2000', 'VRDeck_$2001_30000']
 rescale_col = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck',
                  'Spend_Sum']
 df_spend = df_train[spending_list]
@@ -63,23 +158,31 @@ for col in rescale_col:
 
 #spend accuracy
 lr = LogisticRegression()
-model_accuracy.append(model_accuracy(lr,df_spend, df_train['Transported']))
+model_acc.append(model_accuracy(lr,df_spend, df_train['Transported']))
 model_index.append('Spending Feat')
 
 #spend accuracy with rescale
-model_accuracy.append(model_accuracy(lr,df_spend_scale))
+model_acc.append(model_accuracy(lr,df_spend_scale, df_train['Transported']))
 model_index.append('Spending Rescale Feat')
 
 #checking coefficent of spending not scaled
 lr = LogisticRegression()
 lr.fit(df_spend, df_train['Transported'])
 coefficients = lr.coef_
-spend_feat_importance = pd.Series(coefficients[0],
-                               index=df_spend.columns).sort_values(ascending=False)
+spend_feat_importance = abs(pd.Series(coefficients[0],
+                               index=df_spend.columns)).sort_values(ascending=False)
 high_spend_feat = spend_feat_importance[spend_feat_importance > 0.2]
+
+#Chekcing correlation of spending
+spending_list.append('Transported')
+#Does the correlation of families look like the Logistic Regression coefficent
+df_spend_cor = df_train[spending_list].corr()
+cor_spend_target = df_spend_cor['Transported'].abs().sort_values(ascending=False)
 #RoomService_0, ShoppingMall_0, VRDeck_0 are to close to Spend_Sum_0
-spend_feat = ['Spend_Sum_0','RoomService','ShoppingMall','Spa','VRDeck']
-['RoomService', 'Spa','VRDeck','Spend_Sum_$0']
+#Leaving Spend_sum_0 in because dropping cryo sleep later
+#Best features based on correlation and coefficent of spending
+spend_feat = ['Spend_Sum_0','RoomService','Spa','VRDeck','ShoppingMall', 'FoodCourt']
+
 
 #Looking at age and families
 family_list = ['ID_Group','Age','Age_Missing', 'Age_Infant', 'Age_Child','Age_Teenager',
@@ -88,7 +191,7 @@ family_list = ['ID_Group','Age','Age_Missing', 'Age_Infant', 'Age_Child','Age_Te
                'kids_without_adults']
 
 df_family = df_train[family_list]
-model_accuracy.append(model_accuracy(lr,df_family, df_train['Transported']))
+model_acc.append(model_accuracy(lr,df_family, df_train['Transported']))
 model_index.append('Family Feat')
 
 #checking coefficent of families and ages
@@ -105,34 +208,54 @@ family_list.append('Transported')
 df_family_cor = df_train[family_list].corr()
 cor_family_target = df_family_cor['Transported'].abs().sort_values(ascending=False)
 #Features that share high values in both correlation and lr coefficient
-#ID Group is low but I don't want to part with it yet
-family_feat = ['Age_infant', 'Age_Child', 'family_sum','ID_Group']
+family_feat = ['Age_Infant', 'Age_Child', 'family_sum']
 
 
+#Analyzing remaining features: Destination, Cabin, Home planet and VIP
+#Not analyzing Cryo Sleep becuase it is to close to Spend Sum $0
+status_list = ['VIP', 'HomePlanet_Earth',
+       'HomePlanet_Europa', 'HomePlanet_Mars', 'HomePlanet_Missing',
+       'Destination_55 Cancri e', 'Destination_Missing',
+       'Destination_PSO J318.5-22', 'Destination_TRAPPIST-1e',
+       'Cabin_Deck_A', 'Cabin_Deck_B', 'Cabin_Deck_C', 'Cabin_Deck_D',
+       'Cabin_Deck_E','Cabin_Deck_F', 'Cabin_Deck_G', 'Cabin_Deck_Missing',
+       'Cabin_Deck_T', 'Cabin_Side_Missing', 'Cabin_Side_P', 'Cabin_Side_S']
 
+df_status = df_train[status_list]
+model_acc.append(model_accuracy(lr,df_status, df_train['Transported']))
+model_index.append('Status Feat')
 
+#checking coefficent of status features
+lr = LogisticRegression()
+lr.fit(df_status, df_train['Transported'])
+coefficients = lr.coef_
+status_feat_importance = pd.Series(coefficients[0],
+                               index=df_status.columns).abs().sort_values(ascending=False)
+high_status_feat = status_feat_importance[status_feat_importance > 0.2]
 
+#Checking correlation of status features
+status_list.append('Transported')
+df_status_cor = df_train[status_list].corr()
+cor_status_target = df_status_cor['Transported'].abs().sort_values(ascending=False)
 
+status_features = ['HomePlanet_Earth', 'HomePlanet_Europa', 'Destination_55 Cancri e',
+                   'Destination_TRAPPIST-1e','Cabin_Deck_B',
+                   'Cabin_Deck_C','Cabin_Deck_E','Cabin_Deck_F', 'Cabin_Side_S']
 
-
-
-
-
-
+#Initial list after anazlying the spending, families and status of passengers
 #Using Pearson Correlation to see feature relations to Transported
 #Also refered to as the Filter Method in Feature Selection with sklearn and Pandas
 #Comparing correlation feature values to target column
+init_feat = ['Transported'] + spend_feat + family_feat + status_features
+df_init = df_train[init_feat].copy()
 
-df_copy = df_train.copy()
-
-
-df_cor = df_train.corr()
-cor_target = abs(df_cor['Transported'])
+df_cor = df_init.corr()
+cor_target = abs(df_cor['Transported']).sort_values(ascending=False)
 
 #Selecting highly correlated features
-relevant_features = cor_target[cor_target>0.2]
+relevant_features = cor_target[cor_target>0.1]
 relevant_features_list = relevant_features.index.values.tolist()
-df_relevant = df_train[relevant_features_list]
+df_relevant = df_init[relevant_features_list]
 #Compare features to each other to avoid overfitting model
 #This time dropping the features with high correlation values
 relevant_cor = df_relevant[relevant_features_list].corr()
@@ -146,6 +269,143 @@ for feat_cor in relevant_features_list:
     cor_relevant_high.append(relev_high_list)
     print('Features with high correlation to {}: '.format(feat_cor))
     print(relev_high_list)
+
+#From correlation with Transported was higher with Money spent, 
+#home planet and cabin. Destination and family had a
+#lower correlation with transported. 
+#Home planet Europa has high correlation with Cablin Deck B and C.
+#Spend $0 had higher correlation to the locations money was spent.
+#No other features appear to have high correlation
+
+#Baseline of Model Methods
+x_train=df_init.drop('Transported', axis=1)
+target=df_init['Transported']
+
+acc = ML_Model_FullDF(x_train, target, 'Base')
+print(acc[0])
+print(acc[1])
+
+#Linear Regression
+linr = LinearRegression()
+model_acc.append(model_accuracy(linr,x_train, target))
+model_index.append('Init Linear Regression')
+
+#Lasso Regression with alpha search
+#default alpha=1, best alpha=
+lml = Lasso(alpha=0.1)
+print(model_accuracy(lml,x_train, target))
+
+alpha = []
+error = []
+
+for i in range(1,100):
+    alpha.append(i/1000)
+    lml = Lasso(alpha=(i/1000))
+    error.append(model_accuracy(lml,x_train, target))
+
+plt.plot(alpha, error)
+err = tuple(zip(alpha, error))
+df_err = pd.DataFrame(err, columns = ['alpha', 'error'])
+print(df_err[df_err.error == max(df_err.error)])
+model_acc.append(model_accuracy(lml,x_train, target))
+model_index.append('Init Lasso Regression')
+
+#Random Forest Classifier (grid search)
+hyperparameters = {
+    'criterion' : ['entropy', 'gini'],
+    'max_depth' : [5, 10],
+    'max_features' : ['log2', 'sqrt'],
+    'min_samples_leaf' : [1,5],
+    'min_samples_split' : [3,5],
+    'n_estimators' : [6,9]
+}
+rfc = RandomForestClassifier(random_state=7)
+grid = GridSearchCV(rfc, param_grid=hyperparameters, cv=10)
+grid.fit(x_train, target)
+best_params = grid.best_params_
+best_score = grid.best_score_
+print(best_params)
+print(best_score)
+best_rfc = grid.best_estimator_
+model_acc.append(model_accuracy(best_rfc,x_train, target))
+model_index.append('Init Random Forest Classifier')
+
+#K-Nearest Neighbors
+hyperparameters = {
+    "n_neighbors": range(1,20,2),
+    "weights": ["distance", "uniform"],
+    "algorithm": ['brute'],
+    "p": [1,2]
+}
+knn = KNeighborsClassifier()
+grid = GridSearchCV(knn, param_grid=hyperparameters, cv=10)
+grid.fit(x_train, target)
+print(grid.best_params_)
+print(grid.best_score_)
+best_knn = grid.best_estimator_
+model_acc.append(model_accuracy(best_knn,x_train, target))
+model_index.append('Init K Nearest Neighbors')
+
+
+#From initial dataframe best ML models:
+#Logistic Regression (spending featrues), Random Forest Classifier, K-Nearest Neighbors
+#checking methods on data it would not have seen
+
+train=df_init.drop('Transported', axis=1)
+target=df_init['Transported']
+train_X, test_X, train_y, test_y = train_test_split(
+    train, target, test_size=0.2,random_state=7)
+
+#Logistic Regression
+lr.fit(train_X,train_y)
+predictions = lr.predict(test_X)
+model_acc.append(mean_absolute_error(test_y, predictions))
+model_index.append('Init Unseen LR')
+
+#Random Forest Classifier (grid search)
+hyperparameters = {
+    'criterion' : ['entropy', 'gini'],
+    'max_depth' : [5, 10],
+    'max_features' : ['log2', 'sqrt'],
+    'min_samples_leaf' : [1,5],
+    'min_samples_split' : [3,5],
+    'n_estimators' : [6,9]
+}
+rfc = RandomForestClassifier(random_state=7)
+grid = GridSearchCV(rfc, param_grid=hyperparameters, cv=10)
+grid.fit(x_train, target)
+best_params = grid.best_params_
+best_score = grid.best_score_
+print(best_params)
+print(best_score)
+best_rfc = grid.best_estimator_
+best_rfc.fit(train_X,train_y)
+predictions = best_rfc.predict(test_X)
+model_acc.append(mean_absolute_error(test_y, predictions))
+model_index.append('Init Unseen RFC')
+
+#K-Nearest Neighbors
+hyperparameters = {
+    "n_neighbors": range(1,20,2),
+    "weights": ["distance", "uniform"],
+    "algorithm": ['brute'],
+    "p": [1,2]
+}
+knn = KNeighborsClassifier()
+grid = GridSearchCV(knn, param_grid=hyperparameters, cv=10)
+grid.fit(x_train, target)
+print(grid.best_params_)
+print(grid.best_score_)
+best_knn = grid.best_estimator_
+best_knn.fit(train_X,train_y)
+predictions = best_knn.predict(test_X)
+model_acc.append(mean_absolute_error(test_y, predictions))
+model_index.append('Init Unseen KNN')
+
+
+#Wrapper Method -> Recursive Feature Elemination (RFE) with Cross-Validation (CV)
+
+
 
 #Looking at the relevant_cor data
 #Cryosleep has high correlation with with money being spent.
